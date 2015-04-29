5925bb9915773a741f2a9c1e814b691a6b8d5a4f
Meigne <arnaud.meigne@orange.fr>
2015-04-29 17:47:46 +0200

    2eme commit du projet

---
+++ src/projetjava2/etudiant.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjava2;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class etudiant {
+    System.out.println("test");
+}

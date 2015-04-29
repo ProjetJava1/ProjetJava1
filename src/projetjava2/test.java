5aa18e43a03cde3239bb03b5e3ebd1def89f6a28
Meigne92 <arnaud.meigne@orange.fr>
2015-04-29 18:01:39 +0200

    commit du test

---
+++ src/projetjava2/test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjava2;
+
+/**
+ *
+ * @author Arnaud
+ */
+public class test {
+    
+    public static void main(String[] args) {
+    System.out.println("test");
+    System.out.println("test2");
+    System.out.println("test3");
+
+}
+    
+}
